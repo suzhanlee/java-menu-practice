b4cb1bdd1c0849d5da29bfb1fa55de8016f61c31
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-12-10 23:04:57 +0900

    refactor : Recommendation 반복문 쪼개기로 리팩토링

--- src/main/java/menu/domain/recommend/Recommendation.java
+++ src/main/java/menu/domain/recommend/Recommendation.java
@@ -22,30 +22,43 @@ public class Recommendation {
     }
 
     public List<String> recommend(Couch couch) {
-        Map<Category, Integer> categoryCountRecorder = new LinkedHashMap<>();
-        List<String> menus = new ArrayList<>();
-        for (int recommendationCount = 0; recommendationCount < 5; recommendationCount++) {
-            Category category = randomCategoryRecommender.chooseRandomCategory(4);
-            String menu = randomMenuRecommender.chooseMenuByCategory(category);
-            categoryCountRecorder.put(category, categoryCountRecorder.getOrDefault(category, 0) + 1);
-            menus.add(menu);
-        }
-
+        Map<Category, Integer> categoryCountRecorder = getCategoryCountRecorder();
+        List<String> menus = getMenus(categoryCountRecorder);
         return getRecommendationMenus(couch, menus, categoryCountRecorder);
     }
 
-    private List<String> getRecommendationMenus(Couch couch, List<String> menus,
-                                                Map<Category, Integer> categoryCountRecorder) {
-        RecommendationMenusSupervisor menusSupervisor = new RecommendationMenusSupervisor(
+    private Map<Category, Integer> getCategoryCountRecorder() {
+        Map<Category, Integer> categoryCountRecorder = new LinkedHashMap<>();
+        for (int recommendationCount = 0; recommendationCount < 5; recommendationCount++) {
+            Category category = randomCategoryRecommender.chooseRandomCategory(4);
+            categoryCountRecorder.put(category, categoryCountRecorder.getOrDefault(category, 0) + 1);
+        }
+        return categoryCountRecorder;
+    }
+
+    private List<String> getMenus(Map<Category, Integer> categoryCountRecorder) {
+        List<String> menus = new ArrayList<>();
+        for (Category category : categoryCountRecorder.keySet()) {
+            menus.add(randomMenuRecommender.chooseMenuByCategory(category));
+        }
+        return menus;
+    }
+
+    private List<String> getRecommendationMenus(Couch couch, List<String> menus, Map<Category, Integer> categoryCountRecorder) {
+        if (createRecommendationMenus(couch, menus,
+                categoryCountRecorder).recommendable()) {
+            return menus;
+        }
+        return recommend(couch);
+    }
+
+    private RecommendationMenusSupervisor createRecommendationMenus(Couch couch, List<String> menus, Map<Category, Integer> categoryCountRecorder) {
+        return new RecommendationMenusSupervisor(
                 List.of(
                         new ExcessiveCategoryDuplicationChecker(categoryCountRecorder),
                         new DuplicatedMenusChecker(menus),
                         new DislikedMenuChecker(couch, menus)
                 )
         );
-        if (menusSupervisor.recommendable()) {
-            return menus;
-        }
-        return recommend(couch);
     }
 }
