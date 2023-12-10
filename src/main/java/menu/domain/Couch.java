0283885b3894b4096c6318a07f4e5f6e9cfc5b7b
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-12-10 15:59:18 +0900

    feat : Couch 유효성 검증 기능 추가

---
+++ src/main/java/menu/domain/Couch.java
@@ -0,0 +1,41 @@
+package menu.domain;
+
+import java.util.List;
+
+public class Couch {
+
+    private final String name;
+    private final List<String> dislikeMenus;
+
+    public Couch(String name, List<String> dislikeMenus) {
+        validate(name);
+        validateDislikeMenus(dislikeMenus);
+        this.name = name;
+        this.dislikeMenus = dislikeMenus;
+    }
+
+    private void validate(String name) {
+        validateName(name);
+        validateDislikeMenus(dislikeMenus);
+    }
+
+    private void validateName(String name) {
+        if (outOfNameSizeRange(name)) {
+            throw new IllegalStateException("코치의 이름은 2 ~ 4자리까지 가능합니다.");
+        }
+    }
+
+    private boolean outOfNameSizeRange(String name) {
+        return name.length() < 2 || name.length() > 4;
+    }
+
+    private void validateDislikeMenus(List<String> dislikeMenus) {
+        if (outOfMenusSizeRange(dislikeMenus)) {
+            throw new IllegalStateException("기피 음식은 2개 까지만 지정할 수 있습니다.");
+        }
+    }
+
+    private boolean outOfMenusSizeRange(List<String> dislikeMenus) {
+        return dislikeMenus.size() > 2;
+    }
+}
